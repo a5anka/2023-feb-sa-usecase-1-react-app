63ef7789dfb67530c62e677afa84e369eaa7d8a2
Asanka Abeyweera <asankaab@wso2.com>
2023-02-21 12:36:21 +0530

    Add Login functionality

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 // import logo from './logo.svg';
 import React, { useEffect } from 'react';
+import { AuthProvider, useAuthContext } from "@asgardeo/auth-react";
 import './App.css';
 import './App.scss';
-import { Nav, Navbar, Container }  from 'react-bootstrap';
+import { Nav, Navbar, Container } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
@@ -11,26 +12,29 @@ import Catalog from './components/Catalog/Catalog.js';
 import MyCart from './components/MyCart/Cart.js';
 import Admin from './components/Admin/Admin.js';
 
+
 // Component to render the login/signup/logout menu
 const RightLoginSignupMenu = () => {
+  const { state, signIn, signOut } = useAuthContext();
+
   // Based on Asgardeo SDK, set a variable like below to check and conditionally render the menu
-  let isLoggedIn = false;
+  let isLoggedIn = state.isAuthenticated;
 
   // Host the menu content and return it at the end of the function
   let menu;
 
   // Conditionally render the following two links based on whether the user is logged in or not
   if (isLoggedIn) {
-    menu =  <>
+    menu = <>
       <Nav>
-      <Nav.Link href="#deets">Logout</Nav.Link>
-      <Nav.Link href="#deets"><FontAwesomeIcon icon={faUser} /></Nav.Link></Nav>
+        <Nav.Link href="#deets">Logout</Nav.Link>
+        <Nav.Link href="#deets"><FontAwesomeIcon icon={faUser} /></Nav.Link></Nav>
     </>
   } else {
     menu = <>
       <Nav>
-      <Nav.Link href="#deets">Login</Nav.Link>
-      <Nav.Link href="#deets">Sign Up</Nav.Link></Nav>
+        <Nav.Link href="#deets" onClick={ () => signIn() }>Login</Nav.Link>
+        <Nav.Link href="#deets">Sign Up</Nav.Link></Nav>
     </>
   }
   return menu;
@@ -40,20 +44,20 @@ const RightLoginSignupMenu = () => {
 const PetStoreNav = () => {
   return (
     <>
-    <Navbar bg="light" expand="lg">
-      <Container>
-        <Navbar.Brand href="#home">PetStore</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="/">Catalog</Nav.Link>
-            <Nav.Link href="/mycart">My Cart</Nav.Link>
-            <Nav.Link href="/admin">Admin</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-        <RightLoginSignupMenu />
-      </Container>
-    </Navbar>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">PetStore</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/">Catalog</Nav.Link>
+              <Nav.Link href="/mycart">My Cart</Nav.Link>
+              <Nav.Link href="/admin">Admin</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+          <RightLoginSignupMenu />
+        </Container>
+      </Navbar>
     </>
   );
 };
@@ -64,16 +68,22 @@ const App = () => {
     document.title = 'PetStore';
   }, []);
   return (
-    <>
-    <PetStoreNav />
-    <BrowserRouter>
-      <Switch>
-        <Route path="/" component={Catalog} />
-        <Route path="/mycart" component={MyCart} />
-        <Route path="/admin" component={Admin} />
-      </Switch>
-    </BrowserRouter>
-    </>
+    <AuthProvider config={{
+      signInRedirectURL: "http://localhost:3000",
+      signOutRedirectURL: "http://localhost:3000",
+      clientID: "OSTKXaC1NmXV0oYFIoFkZ7yOLWga",
+      baseUrl: "https://api.asgardeo.io/t/asanka2023febsahackathon",
+      scope: ["openid", "profile"]
+    }}>
+      <PetStoreNav />
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" component={Catalog} />
+          <Route path="/mycart" component={MyCart} />
+          <Route path="/admin" component={Admin} />
+        </Switch>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
